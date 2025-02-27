b68080bd0f0aba699733874385578ba9208cbc2d
Vikar Maulana Arrisyad <vikar.maulana.arrisyad@gmail.com>
2025-02-27 16:12:23 +0700

    menambahkan halaman view artikel all

--- app/Http/Controllers/Front/ArtikelFrontController.php
+++ app/Http/Controllers/Front/ArtikelFrontController.php
@@ -8,6 +8,13 @@ use Illuminate\Http\Request;
 
 class ArtikelFrontController extends Controller
 {
+    public function index()
+    {
+        $artikelList = Artikel::orderBy('tgl_publish', 'desc')->paginate(10);
+
+        return view('frontend.artikel.index', compact('artikelList'));
+    }
+
     // detail artikel
     public function detail($slug)
     {
